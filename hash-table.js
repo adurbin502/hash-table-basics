7f0258797f8c14b8499497f8ebb3a096a3ddca21
Andrew <a.durbin502@gmail.com>
2024-05-15 18:00:46 -0400

    completed

--- hash-table.js
+++ hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,32 +9,75 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
-    // Your code here 
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here 
+    const hashValue = sha256(key).substring(0, 8);
+    return parseInt(hashValue, 16);
   }
 
   hashMod(key) {
-    // Your code here 
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    const index = this.hashMod(key);
+
+    if (this.data[index] === null) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      throw new Error(
+        "hash collision or same key/value pair already exists!"
+      );
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    const index = this.hashMod(key);
+
+    let newPair = new KeyValuePair(key, value);
+
+    if (!this.data[index]) {
+      this.data[index] = newPair;
+    } else {
+      newPair.next = this.data[index];
+      this.data[index] = newPair;
+    }
+    this.count++;
   }
 
   insert(key, value) {
-    // Your code here 
+    const index = this.hashMod(key);
+
+    // Check if the bucket is empty
+    if (this.data[index] === null) {
+      // If the bucket is empty, create a new KeyValuePair
+      this.data[index] = new KeyValuePair(key, value);
+    } else {
+      // If the bucket is not empty, handle collision by chaining elements in a linked list
+      let current = this.data[index];
+      while (current !== null) {
+        if (current.key === key) {
+          // If the same key already exists, update the value
+          current.value = value;
+          return; // Exit the function after updating the value
+        }
+        // Move to the next node in the linked list
+        current = current.next;
+      }
+      // If the key doesn't exist in the linked list, insert a new KeyValuePair at the head
+      const newPair = new KeyValuePair(key, value);
+      newPair.next = this.data[index];
+      this.data[index] = newPair;
+    }
+
+    this.count++;
   }
-
 }
-
 
 module.exports = HashTable;
